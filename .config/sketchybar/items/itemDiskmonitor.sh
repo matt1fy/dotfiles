27be9775590b0d6a1df920e530f0d96aa66fb744
matt1fy <matt.m.mohamed@gmail.com>
2024-07-13 02:25:23 +0800

    Added problematic disk script for skechy

---
+++ .config/sketchybar/items/itemDiskmonitor.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# Created by Matt1fy
+
+diskmonitor=(
+  icon.font="Zed Mono:Regular:24"
+  icon.padding_right=20
+  label.drawing=off
+  y_offset=1
+  update_freq=360
+  updates=when_shown
+  script="$plugin_dir/diskmonitor.sh"
+)
+
+misc=(
+  icon.drawing=off
+  width=0
+  padding_right=4
+  update_freq=360
+  updates=when_shown
+)
+
+diskmonitor_label=(
+  "${misc[@]}"
+  label.font="SF Pro:Semibold:8"
+  label=SSD
+  y_offset=5
+)
+
+diskmonitor_value=(
+  "${misc[@]}"
+  label.font="SF Pro:Bold:10"
+  y_offset=-3
+)
+
+sketchybar                                          \
+  --add item diskmonitor.label right                \
+  --set diskmonitor.label "${diskmonitor_label[@]}" \
+                                                    \
+  --add item diskmonitor.value right                \
+  --set diskmonitor.value "${diskmonitor_value[@]}" \
+                                                    \
+  --add item diskmonitor right                      \
+  --set diskmonitor "${diskmonitor[@]}"             \
+  --subscribe diskmonitor mouse.clicked
--- .config/sketchybar/mResources/mColors.sh
+++ .config/sketchybar/mResources/mColors.sh
@@ -5,47 +5,30 @@
 # mColor Palette
 export BLACK=0xff282828
 export WHITE=0xffebdbb2
-export PURPLE=0xffc46aff
-export BLUE=0xff4b9cff
-export TEAL=0xe679b9a3
-export SPOTIFYGREEN=0xe615e82e
-export HOTPINK=0xe6e02e85
-export SOFTPURPLE=0xfff1e1ff
-export
-export
-export
-export
-export
-export
-export
-export
-export
-export
-export
-
-
-
-
-
-
-export RED=0xffcc241d
-export GREEN=0xff98971a
-export BLUE=0xff458588
-export YELLOW=0xffd79921
-export ORANGE=0xffd65d0e
-export MAGENTA=0xffb16286
 export GREY=0xff928374
 export SKY=0xff83a598
-export PINK=0xffd3869b
+
+export PURPLE=0xffc46aff
+export HOTPINK=0xe6e02e85
+export SOFTPURPLE=0xfff1e1ff
+export MAGENTA=0xffb16286
+export RED=0xffcc241d
+export ORANGE=0xffd65d0e
+export YELLOW=0xffd79921
+export SPOTIFYGREEN=0xe615e82e
+export GREEN=0xff98971a
+export TEAL=0xe679b9a3
+export BRIGHTBLUE=0xff4b9cff
+export BLUE=0xff458588
+
+# My standard accent color
+export ACCENTGOLDI=0xffede0a9
+export ACCENTGOLDII=0xffe8c83f
+export ACCENTGOLDIII=0xffd4a600
+
 export TRANSPARENT=0x00000000
+
 export ITEM_COLOR=0xff32302f
-
-# General bar colors
-export BAR_COLOR=0x703c3836 #$GREY # Grey bar
-export ICON_COLOR=$WHITE # Color of all icons
-export LABEL_COLOR=$WHITE # Color of all labels
-export ALT_LABEL_COLOR=$SOFTPURPLE # Alternative label color
-
 export SPACE_BACKGROUND=$ITEM_COLOR
 export SPACE_BACKGROUND2=0xff282828
 export SPACE_SELECTED=$WHITE
@@ -53,7 +36,13 @@ export SPACE_DESELECTED=$BLACK
 export OPEN_APPS_BACKGROUND=$ITEM_COLOR
 export CALENDAR_BACKGROUND=0xffb8c0e0
 
-export POPUP_BACKGROUND_COLOR=$BLACK
-export POPUP_BORDER_COLOR=$WHITE
+# General bar colors
+export BAR_COLOR=0x703c3836 #$GREY # Grey bar
+export ICON_COLOR=$WHITE # Color of all icons
+export LABEL_COLOR=$WHITE # Color of all labels
+export ALT_LABEL_COLOR=$SOFTPURPLE # Alternative label color
+
+export POPUP_BG_COLOR=$BLACK
+export POPUP_BD_COLOR=$WHITE
 
 export SHADOW_COLOR=$BLACK
